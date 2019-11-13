df795e4b921cb22019b418b13b1aeb4055c7fde7
jxustcmh <cmh960808>
2019-11-13 21:41:59 +0800

    最长公共前缀

--- src/com/hx/arr/Array.java
+++ src/com/hx/arr/Array.java
@@ -10,16 +10,45 @@ import java.util.HashMap;
 public class Array {
     public static void main(String[] args) {
         Array array = new Array();
-       // System.out.println(array.isPalindrome(1));
-        int []nums = { 7, 11, 15,2};
+        // System.out.println(array.isPalindrome(1));
+/*        int[] nums = {7, 11, 15, 2};
         int target = 9;
         int[] result = array.twoSum(nums, target);
-        System.out.println(result[0]+"  "+result[1]);
+        System.out.println(result[0] + "  " + result[1]);*/
+
+        String[] longarr = {"flower", "flow", "flight"};
+        System.out.println(array.longestCommonPrefix(longarr));
+
+    }
+
+    /**
+     * 最长公共前缀
+     *
+     * @param strs 字符串数组
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        // 假设字符数组的第一个元素为最长公共前缀
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            // 后边字符串出现的位子
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.length() == 0) {
+                    return "";
+                }
+            }
+
+        }
+        return prefix;
     }
 
 
-
     /**
+     * 两数之和
      *
      * @param nums
      * @param target
