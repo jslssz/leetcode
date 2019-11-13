7d14aa7b2c762ab00c5e0e71a53202c5ebd09a4f
jxustcmh <cmh960808>
2019-11-13 22:45:14 +0800

    有效括号

--- src/com/hx/classic/Classic.java
+++ src/com/hx/classic/Classic.java
@@ -7,86 +7,124 @@ package com.hx.classic;
  */
 public class Classic {
     public static void main(String[] args) {
-        Classic classic =new Classic();
-        String romanString =  "MCMXCIV";
-        System.out.println(classic.romanToInt(romanString));
+        Classic classic = new Classic();
+//        String romanString =  "MCMXCIV";
+//        System.out.println(classic.romanToInt(romanString));
 
+        String s3 = "{{{}}}";
+        System.out.println(classic.isValid(s3));
 
     }
 
-        /**
-         * 罗马数字转整数
-         * 解题思路：
-         * 1. 依次迭代s,如果前后两个字符不一致，且非subtraction，进行次数累乘。如为subtraction取特别值。
-         * @param s 入参
-         * @return 转换数值
-         */
-        public int romanToInt(String s) {
-            char[] chars = s.toCharArray();
-            int result = 0;
-            int length = chars.length;
-            for (int i = 0; i < length; i++) {
-                char c = chars[i];
-                switch (c) {
-                    case 'M':
-                        result += 1000;
-                        break;
-                    case 'D':
-                        result += 500;
-                        break;
-                    case 'C':
-                        if (i < length - 1) {
-                            if (chars[i + 1] == 'M') {
-                                result += 900;
-                                i++;
-                                break;
-                            } else if (chars[i + 1] == 'D') {
-                                result += 400;
-                                i++;
-                                break;
-                            }
-                        }
-                        result += 100;
-                        break;
-                    case 'L':
-                        result += 50;
-                        break;
-                    case 'X':
-                        if (i < length - 1) {
-                            if (chars[i + 1] == 'C') {
-                                result += 90;
-                                i++;
-                                break;
-                            } else if (chars[i + 1] == 'L') {
-                                result += 40;
-                                i++;
-                                break;
-                            }
-                        }
-                        result += 10;
-                        break;
-                    case 'V':
-                        result += 5;
-                        break;
-                    default:
-                        // I
-                        if (i < length - 1) {
-                            if (chars[i + 1] == 'X') {
-                                result += 9;
-                                i++;
-                                break;
-                            } else if (chars[i + 1] == 'V') {
-                                result += 4;
-                                i++;
-                                break;
-                            }
-                        }
-                        result += 1;
-                        break;
-                }
-            }
-            return result;
+
+    /**
+     * 有效的括号 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效
+     *
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        // 边界条件的判断
+        if (s == null || s.length() % 2 == 1) {
+            return false;
         }
+        if (s.length() == 0) {
+            return true;
+        }
+        int stackIndex = 0;
+        char[] data = s.toCharArray();
+        char[] stack = new char[data.length];
+        stack[0] = '0';
+
+        for (char item : data) {
+            if (item == '[' || item == '{' || item == '(') {
+                stack[stackIndex++] = item;
+            } else if (stackIndex == 0 || item == ']' && stack[stackIndex - 1] != '['
+                    || item == '}' && stack[stackIndex - 1] != '{'
+                    || item == ')' && stack[stackIndex - 1] != '(') {
+                return false;
+            } else {
+                stack[--stackIndex] = '0';
+            }
+        }
+        return stackIndex == 0;
+    }
+
+
+    /**
+     * 罗马数字转整数
+     * 解题思路：
+     * 1. 依次迭代s,如果前后两个字符不一致，且非subtraction，进行次数累乘。如为subtraction取特别值。
+     *
+     * @param s 入参
+     * @return 转换数值
+     */
+    public int romanToInt(String s) {
+        char[] chars = s.toCharArray();
+        int result = 0;
+        int length = chars.length;
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+            switch (c) {
+                case 'M':
+                    result += 1000;
+                    break;
+                case 'D':
+                    result += 500;
+                    break;
+                case 'C':
+                    if (i < length - 1) {
+                        if (chars[i + 1] == 'M') {
+                            result += 900;
+                            i++;
+                            break;
+                        } else if (chars[i + 1] == 'D') {
+                            result += 400;
+                            i++;
+                            break;
+                        }
+                    }
+                    result += 100;
+                    break;
+                case 'L':
+                    result += 50;
+                    break;
+                case 'X':
+                    if (i < length - 1) {
+                        if (chars[i + 1] == 'C') {
+                            result += 90;
+                            i++;
+                            break;
+                        } else if (chars[i + 1] == 'L') {
+                            result += 40;
+                            i++;
+                            break;
+                        }
+                    }
+                    result += 10;
+                    break;
+                case 'V':
+                    result += 5;
+                    break;
+                default:
+                    // I
+                    if (i < length - 1) {
+                        if (chars[i + 1] == 'X') {
+                            result += 9;
+                            i++;
+                            break;
+                        } else if (chars[i + 1] == 'V') {
+                            result += 4;
+                            i++;
+                            break;
+                        }
+                    }
+                    result += 1;
+                    break;
+            }
+        }
+        return result;
+    }
 
 
     /**
