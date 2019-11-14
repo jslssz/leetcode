bad71f216c77f9705e79cd7a803cfe3b6e1c2084
jxustcmh <cmh960808>
2019-11-14 09:30:21 +0800

    数组去重,删除重复项

--- src/com/hx/arr/Array.java
+++ src/com/hx/arr/Array.java
@@ -11,15 +11,58 @@ public class Array {
     public static void main(String[] args) {
         Array array = new Array();
         // System.out.println(array.isPalindrome(1));
-/*        int[] nums = {7, 11, 15, 2};
-        int target = 9;
-        int[] result = array.twoSum(nums, target);
-        System.out.println(result[0] + "  " + result[1]);*/
+        int[] nums = {7, 11, 11, 2,11,11};
+        int target = 11;
+//        int[] result = array.twoSum(nums, target);
+//        System.out.println(result[0] + "  " + result[1]);
 
-        String[] longarr = {"flower", "flow", "flight"};
-        System.out.println(array.longestCommonPrefix(longarr));
+//        String[] longarr = {"flower", "flow", "flight"};
+//        System.out.println(array.longestCommonPrefix(longarr));
+
+        System.out.println(array.removeElement(nums, target));
 
     }
+
+    /**
+     *  给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int len = nums.length;
+        // 注意++条件的控制
+        for (int i = 0; i < len;) {
+            if (nums[i] == val) {
+                nums[i] = nums[len - 1];
+                len--;
+            } else {
+                i++;
+            }
+        }
+        return len;
+    }
+
+
+    /**
+     *  删除排序数组中的重复项
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+            int len=nums.length;
+            if (len ==0) {
+                return 0;
+            }
+            int i=0;
+            for(int j=1;j<len;j++) {
+                if (nums[i] !=nums[j]) {
+                    i++;
+                    nums[i]=nums[j];
+                }
+            }
+            return i+1;
+        }
 
     /**
      * 最长公共前缀
@@ -56,16 +99,16 @@ public class Array {
      */
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>(nums.length);
-        int[] ans = new int[2];
+        int[] len = new int[2];
         for (int i = 0; i < nums.length; i++) {
             int res = target - nums[i];
             if (map.get(res) != null) {
-                ans[0] = map.get(res);
-                ans[1] = i;
-                return ans;
+                len[0] = map.get(res);
+                len[1] = i;
+                return len;
             }
             map.put(nums[i], i);
         }
-        return ans;
+        return len;
     }
 }
