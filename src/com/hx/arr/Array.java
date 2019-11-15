f0fa193846288a680539757fe7216e59304438c4
jxustcmh <cmh960808>
2019-11-15 10:18:11 +0800

    add

--- src/com/hx/arr/Array.java
+++ src/com/hx/arr/Array.java
@@ -27,9 +27,31 @@ public class Array {
 //        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 //        System.out.println(array.maxSubArray(arr));
 
-        int [] arr2 ={9,9,9};
+        int[] arr2 = {9, 9, 9};
         System.out.println(Arrays.toString(array.plusOne(arr2)));
 
+    }
+
+    /**
+     *  给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+     *  初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+     * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int sumLen = m + n - 1;
+        int arr1Len = m - 1;
+        int arr2Len = n - 1;
+        // 根据题意:arr1Len >arr2Len,并且sumLen =arr1Len +arr2Len
+        while (arr1Len >= 0 && arr2Len >= 0){
+            nums1[sumLen--] = nums1[arr1Len] > nums2[arr2Len] ? nums1[arr1Len--] : nums2[arr2Len--];
+        }
+        while (arr2Len >= 0){
+            nums1[sumLen--] = nums2[arr2Len--];
+        }
     }
 
 
--- src/com/hx/list/List.java
+++ src/com/hx/list/List.java
@@ -22,6 +22,25 @@ public class List {
         list.mergeTwoLists(listNode1,listNode4);
     }
 
+
+    /**
+     * 给定一个<<排序>>链表，删除所有重复的元素，使得每个元素只出现一次。
+     * @param head 1->1->2->3->3
+     * @return  1->2->3
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode next = pre.next;
+            while (next != null && next.val == pre.val) {
+                next = next.next;
+            }
+            pre.next = next;
+            pre = next;
+        }
+        return head;
+    }
+
     /**
      *将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
      * @param l1 链表1
